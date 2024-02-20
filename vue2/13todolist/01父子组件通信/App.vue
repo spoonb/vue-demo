98ae9f00e4c1ec1f648ef82917298c18c5cc2a08
spoonb <q6666221@gmail.com>
2024-02-20 10:07:41 +0900

    01父子组件通信

---
+++ vue2/13todolist/01父子组件通信/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <ToDoAppend :addToDoRecord="addToDoRecord" />
+    <ToDoList :todos="todos" :toggleCheck="toggleCheck" :delToDo="delToDo" />
+    <ToDoTotal
+      v-show="total"
+      :current="current"
+      :total="total"
+      :isAll="isAll"
+      :toggleCheckAll="toggleCheckAll"
+      :delToDoAll="delToDoAll"
+    />
+  </div>
+</template>
+
+<script>
+import ToDoAppend from "./components/ToDo/ToDoAppend";
+import ToDoList from "./components/ToDo/ToDoList";
+import ToDoTotal from "./components/ToDo/ToDoTotal";
+import { nanoid } from "nanoid";
+
+export default {
+  name: "App",
+  components: {
+    ToDoAppend,
+    ToDoList,
+    ToDoTotal,
+  },
+  data() {
+    return {
+      todos: [
+        { id: "001", title: "抽烟", completed: false },
+        { id: "002", title: "喝酒", completed: true },
+        { id: "003", title: "烫头", completed: false },
+      ],
+    };
+  },
+  methods: {
+    addToDoRecord(value) {
+      if (!value.trim()) return;
+      this.todos.unshift({ id: nanoid(), title: value, completed: false });
+    },
+    toggleCheck(id) {
+      this.todos.forEach((e) => {
+        if (e.id === id) {
+          e.completed = !e.completed;
+          return;
+        }
+      });
+    },
+    delToDo(id) {
+      this.todos = this.todos.filter((e) => e.id !== id);
+    },
+    toggleCheckAll(value) {
+      this.todos.forEach((e) => (e.completed = value));
+    },
+    delToDoAll() {
+      this.todos = this.todos.filter((e) => !e.completed);
+    },
+  },
+  computed: {
+    total() {
+      return this.todos.length;
+    },
+    current() {
+      // pre: 前一次的迭代值, current: 迭代的当前元素
+      return this.todos.reduce((pre, current) => {
+        if (current.completed) return pre + 1;
+        return pre;
+      }, 0);
+    },
+    isAll() {
+      return this.total > 0 && this.current === this.total;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue2/13todolist/01父子组件通信/components/ToDo/ToDoTotal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <label>
+      <input type="checkbox" v-model="isChecked" @click="toggleCheckAll(!completed)" />
+      <span>已完成{{ current }}/总计{{ total }}</span>
+      <button @click="delToDoAll">删除已完成事项</button>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDoTotal",
+  data() {
+    return {
+      completed: false,
+    };
+  },
+  props: ["current", "total", "isAll", "toggleCheckAll", "delToDoAll"],
+  computed: {
+    isChecked: {
+      get() {
+        return this.isAll && this.completed
+      },
+      set(value) {
+        this.completed = value
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
