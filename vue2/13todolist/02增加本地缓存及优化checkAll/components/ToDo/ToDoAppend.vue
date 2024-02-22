667747b9a892734f22d9eeb64521875d596e65d2
spoonb <q6666221@gmail.com>
2024-02-22 11:46:17 +0900

    02增加本地缓存及优化checkAll

---
+++ vue2/13todolist/02增加本地缓存及优化checkAll/components/ToDo/ToDoAppend.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" v-model="value"
+     @keyup.enter.stop="addToDoRecord(value)" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDoSearchAppend",
+  data() {
+    return {
+      value: "",
+    };
+  },
+  props: ["addToDoRecord"],
+};
+</script>
+
+<style>
+</style>
---
+++ vue2/13todolist/02增加本地缓存及优化checkAll/components/ToDo/ToDoRecord.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <label>
+      <input type="checkbox" :checked="todo.completed" @click="toggleCheck(todo.id)" />
+      <span>{{ todo.title }}</span>
+    </label>
+    <button @click="delToDo(todo.id)">刪除</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "ToDoRecord",
+  props: ["todo", "toggleCheck", "delToDo"],
+};
+</script>
+
+<style>
+li {
+  list-style: none;
+}
+</style>
