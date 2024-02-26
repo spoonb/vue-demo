7ad9813315a85e3fe1cff376effc2ab272c49ce2
spoonb <q6666221@gmail.com>
2024-02-27 01:03:05 +0900

    03子向父传数据(自定义事件)

---
+++ vue2/51todolist/03子向父传数据(自定义事件)/components/ToDo/ToDoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <ToDoRecord
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      :toggleCheck="toggleCheck"
+      :delToDo="delToDo"
+    />
+  </ul>
+</template>
+
+<script>
+import ToDoRecord from "./ToDoRecord";
+
+export default {
+  name: "ToDoList",
+  components: {
+    ToDoRecord,
+  },
+  props: ["todos", "toggleCheck", "delToDo"],
+};
+</script>
+
+<style>
+ul {
+  padding-left: 0px;
+}
+</style>
