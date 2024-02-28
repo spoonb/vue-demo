da6559d38543a9dce748d3e99625913e0c95a0ff
spoonb <q6666221@gmail.com>
2024-02-28 13:27:18 +0900

    04事件总线(任意组件间传数据)

---
+++ vue2/51todolist/04事件总线(任意组件间传数据)/components/ToDo/ToDoAppend.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="text" v-model="value"
+     @keyup.enter.stop="addToDoRecord" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDoSearchAppend",
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    addToDoRecord() {
+      this.$emit("addToDoRecord", this.value)
+    }
+  }
+};
+</script>
+
+<style>
+</style>
